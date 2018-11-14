ff504733e29b207563bd4768bb9e3dd2e7d0aeb0
Jeroen Jonker <jeroenojonker@hotmail.nl>
2018-11-14 10:42:28 +0100

    test of google api

--- BlazorAgenda/BlazorAgenda.Server/Controllers/SampleDataController.cs
+++ BlazorAgenda/BlazorAgenda.Server/Controllers/SampleDataController.cs
@@ -1,8 +1,16 @@
 ﻿using BlazorAgenda.Shared;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BlazorAgenda.Server.Controllers
@@ -10,6 +18,8 @@ namespace BlazorAgenda.Server.Controllers
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -25,6 +35,63 @@ namespace BlazorAgenda.Server.Controllers
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             });
+        }
+        [HttpGet("[action]")]
+        public string Test()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    return string.Format("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                return string.Format("No upcoming events found.");
+            }
+            //AuthCallbackController a = new AuthCallbackController();
+            return "succes";
         }
     }
 }
