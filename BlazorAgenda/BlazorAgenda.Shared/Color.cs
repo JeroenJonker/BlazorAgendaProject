7ef54ea57799224d6617e2aba660d501ec5c1a7f
dennyo <bijl1507@student.nhl.nl>
2018-11-20 15:46:06 +0100

    Show Events and Colors in CalendarView

---
+++ BlazorAgenda/BlazorAgenda.Shared/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorAgenda.Shared
+{
+    public class Color : BaseObject
+    {
+        public string ColorId { get; set; }
+        public string Background { get; set; }
+        public string Foreground { get; set; }
+    }
+}
