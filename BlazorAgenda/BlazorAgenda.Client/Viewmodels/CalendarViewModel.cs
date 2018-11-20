308fd5fd6ca0ec2b284fa5a391c3e1209b99ca08
dennyo <bijl1507@student.nhl.nl>
2018-11-20 11:31:50 +0100

    Create calendar table as Dynamic Component

--- BlazorAgenda/BlazorAgenda.Client/Viewmodels/CalendarViewModel.cs
+++ BlazorAgenda/BlazorAgenda.Client/Viewmodels/CalendarViewModel.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Blazor;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.RenderTree;
 
 namespace BlazorAgenda.Client.Viewmodels
 {
@@ -14,22 +15,90 @@ namespace BlazorAgenda.Client.Viewmodels
         [Inject]
         protected CalendarService Service { get; set; }
         [Parameter]
-        private bool Loaded { get; set; }
+        protected bool Loaded { get; set; }
         [Parameter]
         protected Action<bool> LoadedChanged { get; set; }
 
         public List<CalendarEvent> Events { get; set; }
-        public DateTime CurrentDate { get; set; }
+        public DateTime CurrentDate { get; set; } = DateTime.Today;
         public DateTime StartOfWeekDate { get; set; }
+
+        public RenderFragment Head { get; set; }
+        public RenderFragment Body { get; set; }
 
         public async Task GetAsync()
         {
             Events = await Service.GetAsync();
-            CurrentDate = DateTime.Today;
+        }
+
+        public void BuildTable()
+        {
             int delta = DayOfWeek.Monday - CurrentDate.DayOfWeek;
             if (delta > 0)
                 delta -= 7;
             StartOfWeekDate = CurrentDate.AddDays(delta);
+            int today = CurrentDate.Day;
+            int start = StartOfWeekDate.Day;
+            Head = builder =>
+            {
+                int seq = 0;
+                builder.OpenElement(seq, "tr");
+                builder.OpenElement(++seq, "th");
+                builder.CloseElement();
+                for(int col = start; col < start + 7; col++)
+                {
+                    DateTime day = StartOfWeekDate.AddDays(col - start);
+                    string name = day.ToString("dddd");
+
+                    builder.OpenElement(++seq, "th");
+                    string columnClass = (col == today) ? "day active" : "day";
+                    builder.OpenElement(++seq, "span");
+                    builder.AddAttribute(++seq, "class", columnClass);
+                    builder.AddContent(++seq, col.ToString());
+                    builder.CloseElement();
+                    builder.OpenElement(++seq, "span");
+                    builder.AddAttribute(++seq, "class", "long");
+                    builder.AddContent(++seq, name);
+                    builder.CloseElement();
+                    builder.OpenElement(++seq, "span");
+                    builder.AddAttribute(++seq, "class", "short");
+                    builder.AddContent(++seq, name.Substring(0, 3));
+                    builder.CloseElement();
+                    builder.CloseElement();
+                }
+                builder.CloseElement();
+            };
+            Body = builder =>
+            {
+                int seq = 0;
+                for (int hour = 0; hour < 24; hour++)
+                {
+                    builder.OpenElement(++seq, "tr");
+                    builder.OpenElement(++seq, "td");
+                    builder.AddAttribute(++seq, "class", "hour");
+                    builder.AddAttribute(++seq, "rowspan", "4");
+                    builder.OpenElement(++seq, "span");
+                    builder.AddContent(++seq, hour.ToString() + ":00");
+                    builder.CloseElement();
+                    builder.CloseElement();
+                    for (int col = 0; col < 7; col++)
+                    {
+                        builder.OpenElement(++seq, "td");
+                        builder.CloseElement();
+                    }
+                    builder.CloseElement();
+                    for (int row = 0; row < 3; row++)
+                    {
+                        builder.OpenElement(++seq, "tr");
+                        for(int innerCol = 0; innerCol < 7; innerCol++)
+                        {
+                            builder.OpenElement(++seq, "td");
+                            builder.CloseElement();
+                        }
+                        builder.CloseElement();
+                    }
+                }
+            };
             Loaded = true;
             LoadedChanged?.Invoke(Loaded);
         }
