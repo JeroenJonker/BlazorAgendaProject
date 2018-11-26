47bb08fc7f78d5ff253c9f4de59451826772290d
Jeroen Jonker <jeroenojonker@hotmail.nl>
2018-11-26 15:51:02 +0100

    Fixed Date overriding Time

--- BlazorAgenda/BlazorAgenda.Client/Viewmodels/CalendarEventViewmodel.cs
+++ BlazorAgenda/BlazorAgenda.Client/Viewmodels/CalendarEventViewmodel.cs
@@ -11,13 +11,47 @@ namespace BlazorAgenda.Client.Viewmodels
 {
     public class CalendarEventViewmodel : BlazorComponent
     {
-        //[Parameter]
-        //protected CalendarEvent CalendarEvent { get; set; }
-
         [Inject]
         protected ICalendarEventService Service { get; set; }
+        public DateTime Start
+        {
+            get
+            {
+                return Service.CurrentObject.Start;
+            }
+            set
+            {
+                if (Service.CurrentObject.Start.Date != value.Date)
+                {
+                    Service.CurrentObject.Start = new DateTime(value.Year, value.Month, value.Day, 
+                        Service.CurrentObject.Start.Hour, Service.CurrentObject.Start.Minute, Service.CurrentObject.Start.Second);
+                }
+                else
+                {
+                    Service.CurrentObject.Start = value;
+                }
+            }
+        }
 
-        //[Parameter] protected Action<BaseObject> Post { get; set; }
+        public DateTime End
+        {
+            get
+            {
+                return Service.CurrentObject.End;
+            }
+            set
+            {
+                if (Service.CurrentObject.End.Date != value.Date)
+                {
+                    Service.CurrentObject.End = new DateTime(value.Year, value.Month, value.Day, 
+                        Service.CurrentObject.End.Hour, Service.CurrentObject.End.Minute, Service.CurrentObject.End.Second);
+                }
+                else
+                {
+                    Service.CurrentObject.End = value;
+                }
+            }
+        }
 
         protected override void OnInit()
         {
