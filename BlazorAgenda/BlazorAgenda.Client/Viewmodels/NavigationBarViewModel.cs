b7b422115ce99b06d5626b06ea762cc85e01c6d0
Jeroen Jonker <jeroenojonker@hotmail.nl>
2018-12-12 14:36:49 +0100

    Added Viewmodel to NavigationBar

---
+++ BlazorAgenda/BlazorAgenda.Client/Viewmodels/NavigationBarViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BlazorAgenda.Services.Interfaces;
+using BlazorAgenda.Shared.Models;
+using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAgenda.Client.Viewmodels
+{
+    public class NavigationBarViewModel : BlazorComponent
+    {
+        [Inject] protected IStateService StateService { get; set; }
+
+        public void EditUser()
+        {
+            StateService.ObjectFocus = typeof(User);
+        }
+
+        public void OnCloseDialog()
+        {
+            StateService.ObjectFocus = null;
+        }
+
+        public void Logout()
+        {
+            StateService.ResetState();
+            StateService.NotifyStateChanged();
+        }
+    }
+}
