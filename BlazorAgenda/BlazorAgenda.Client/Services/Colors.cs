5286c3ab32054f8bfe042ed8f4ec504a392ede6d
dennyo <bijl1507@student.nhl.nl>
2018-12-17 15:54:23 +0100

    Toggle user events with checkbox + different colors per user

---
+++ BlazorAgenda/BlazorAgenda.Client/Services/Colors.cs
@@ -0,0 +1,13 @@
+﻿using BlazorAgenda.Shared.Models;
+using System.Collections.Generic;
+
+namespace BlazorAgenda.Client.Services
+{
+    public static class Colors
+    {
+        public static string[] Items { get; } = new string[]
+        {
+            "#a4bdfc", "#7ae7bf", "#dbadff", "#ff887c", "#fbd75b", "#ffb878", "#46d6db", "#e1e1e1", "#5484ed", "#51b749", "#dc2127"
+        };
+    }
+}
